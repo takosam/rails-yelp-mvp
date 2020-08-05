8a27195cbcc5f0953cd7989633b32fa3865bd317
Samir Bennafla <samir.bennafla@gmail.com>
2020-08-05 17:02:11 +0900

    APP restaurant Yelp App

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...MFK'
+Restaurant.destroy_all
+puts 'Creating restaurants...MFK'
+50.times do
+  Restaurant.create!(
+    name: Faker::Games::Witcher.character,
+    address: Faker::Address.street_address,
+    category: Restaurant::CATEGORY.sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+end
+puts "Created #{Restaurant.count} restaurants..."
